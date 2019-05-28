b4b5733107cedae88e93ee4848feb190b72f6f6d
qq240814476 <240814476@qq.com>
2019-05-28 23:13:09 +0800

    api

--- src/api.py
+++ src/api.py
@@ -1,3 +1,78 @@
 #发送登录连接
+def login():
+    #点击当前页面的"登录"
+    bwr.find_by_text(u"登录").click()
+    sleep(3)
+    #fill填充搜索框的内容，username。name=loginUserDTO.user_name的元素。
+    bwr.fill("loginUserDTO.user_name", username)
+    sleep(1)
+    bwr.fill("userDTO.password", passwd)
+    sleep(1)
+    print(u"等待验证码，自行输入...")
+    #登录手动输入验证码，并登录系统
+    while True:
+        #判断当前的url是否已经进入系统
+        if bwr.url != initmy_url:
+            sleep(1)
+        else:
+            break
 
 #发送抢票连接
+def getTickt():
+    global bwr
+    # 使用splinter打开chrome浏览器
+    bwr=Browser(driver_name="chrome")
+    # splinter打开浏览器（返回购票页面）
+    bwr.visit(ticket_url)
+    while bwr.is_text_present(u"登录"):
+        sleep(1)
+        login()
+        #判断是否已经进入系统
+        if bwr.url == initmy_url:
+            break
+    try:
+        print(u"购票页面...")
+        # splinter打开浏览器（跳回购票页面）
+        bwr.visit(ticket_url)
+        # 加载查询信息
+        bwr.cookies.add({"_jc_save_fromStation": from_station})
+        bwr.cookies.add({"_jc_save_toStation": to_station})
+        bwr.cookies.add({"_jc_save_fromDate": from_date})
+        bwr.reload()
+        sleep(2)
+        count=0
+        # 循环点击预订
+        if order != 0:
+            while bwr.url == ticket_url:
+                bwr.find_by_text(u"查询").click()
+                count += 1
+                print(u"循环点击查询... 第 %s 次" % count)
+                sleep(1)
+                try:
+                    bwr.find_by_text(u"预订")[order - 1].click()
+                except:
+                    print(u"还没开始预订")
+                    continue
+        else:
+            while bwr.url == ticket_url:
+                 bwr.find_by_text(u"查询").click()
+                 count += 1
+                 print(u"循环点击查询... 第 %s 次" % count)
+                 sleep(1)
+                 try:
+                     for i in bwr.find_by_text(u"预订"):
+                         i.click()
+                         sleep(1)
+                 except:
+                     print(u"还没开始预订")
+                     continue
+        sleep(1)
+        # 可以通过修改sleep的参数来调整延时, 但延时不要太低, 防止被12306网站认为是刷票屏蔽掉.
+        bwr.find_by_text(ticketer)[0].click()
+        sleep(1)
+        bwr.find_by_text(u"提交订单").click()
+        sleep(1)
+        #bwr.find_by_id(u"qr_submit_id").click()
+        print(u"成功抢到一张宝贵的票")
+    except Exception as e:
+         print(traceback.print_exc())
